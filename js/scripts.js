7460fca99b38a6c6d14e8d980e45bea8c4e5ea51
sam-flores <55456019+PINKPHANTOMS@users.noreply.github.com>
2022-07-23 16:47:00 -0700

    fdsa

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,39 @@ $( document ).ready(function() {
     var src = "";
     var weeks = ["one", "two", "three", "four"];
     var days = ["one", "two", "three", "four", "five", "six", "seven"];
+    
     // loop through weeks
-    $.each(weeks, function (indexx, valuee) {
+    $.each(weeks, function (week, valuee) {
+
+      src += '<div id="week'+week+'" class="btn-group" role="group" aria-label="Basic Example"></div><div id="flyers'+week+'"></div>'
+    });
+
+    // add src to collapse month
+    $("#collapse_month").html(src);
+
+    //reset src
+    src = "";
+
+    $.each(weeks, function (week, valuee) {
 
       // loop through days
       $.each(days, function (index, value) {
-          src += '<button id="'+indexx+index+'">day'+indexx+index+'</button>';
+          src += '<button id="'+week+index+'">day'+week+index+'</button>';
       });
 
       // add src to week div
-      $("#week"+indexx).html(src);
+      $("#week"+week).html(src);
 
       // reset src
       src = "";
 
       // add a flyer section underneath each day
       $.each(days, function (index, value) {
-          src += '<div style="color: white; display: none;" id="day'+indexx+index+'"></div>';
+          src += '<div style="color: white; display: none;" id="day'+week+index+'"></div>';
       });
 
       // add src to flyer divs
-      $("#flyers"+indexx).html(src);
+      $("#flyers"+week).html(src);
 
       src = "";
       // hard coded for now
@@ -33,12 +45,12 @@ $( document ).ready(function() {
       // loop through days of week
       $.each(days, function (index, value) {
         // loop through flyers for day
-        src = '<div style="color: white; font-size: 32px;">day'+indexx+index+'</div>';
+        src = '<div style="color: white; font-size: 32px;">day'+week+index+'</div>';
         $.each(flyers, function (flyer, val) {
           src +='<div class="col-md-4 mb-5"><div class="card h-100 bg-dark"><div class="card-body"><img class="img-fluid rounded mb-4 mb-lg-0" src="./assets/epdx_logo.png" alt="..." /><h2 class="card-title text-light">GAY BEHAVIOR: DJ SOUP and EMOJI HEAP</h2><p class="card-text text-success">gay people. gay music. all you need for your queer desires.</p></div><div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">more info</a><a class="btn btn-primary btn-sm" href="#!">save event</a></div></div></div>';
         });
         // add flyers to day
-        $("#day"+indexx+index).html(src);
+        $("#day"+week+index).html(src);
         src = "";// reset source for next day
       });
     });
@@ -50,17 +62,17 @@ $( document ).ready(function() {
       if($("#day" + e.target.id).is(":visible")){
         $("#day" + e.target.id).hide();
         // show all previously hidden weeks
-        $.each(weeks, function (indexx, valuee) {
-          $("#week" + indexx).show();
+        $.each(weeks, function (week, valuee) {
+          $("#week" + week).show();
         });
       }
       // shift to week view
       else{
-      $.each(weeks, function (indexx, valuee) {
+      $.each(weeks, function (week, valuee) {
         // hide all weeks except for selected
-        if(indexx.toString() != e.target.id.substring(0,1)) $("#week" + indexx).hide();
+        if(week.toString() != e.target.id.substring(0,1)) $("#week" + week).hide();
         $.each(days, function (index, value) {
-          $("#day" + indexx+index).hide();
+          $("#day" + week+index).hide();
         });
       });
       // scroll to flyers animation
